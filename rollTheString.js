e4566c29b91007067c1c36c50d38ab773fa69253
RaymondwChen <70980012+RaymondwChen2@users.noreply.github.com>
2021-10-07 21:21:28 -0700

    leetcode practice

---
+++ rollTheString.js
@@ -0,0 +1,13 @@
+// Input: s = "bca"
+// roll[] = { 1, 2, 3}
+// Output: eeb
+
+// Explanation:
+// arr[0] = 1 means roll first character of string -> cca
+// arr[1] = 2 means roll first two characters of string -> dda
+// arr[2] = 3 means roll first three characters of string -> eeb
+// So final ans is "eeb"
+
+// Input: s = "zcza"
+// roll[] = { 1, 1, 3, 4}
+// Output: debb
