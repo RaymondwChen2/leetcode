9198ae1a8e811cd7c53d0605498c9eb8d518f90c
RaymondwChen <70980012+RaymondwChen2@users.noreply.github.com>
2021-11-17 15:42:07 -0800

    leetcode

--- valid_parentheses.js
+++ valid_parentheses.js
@@ -1,5 +1,19 @@
 var isValid = function(s){
+    let stack = [];
 
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+        } else if ((char === ']' && stack[stack.length - 1] === '[') || (char === ')' && stack[stack.length - 1] === '(') || (char === '}' && stack[stack.length - 1] === '{')) {
+            stack.pop();
+        }// } else {
+        //     return false;
+        // }
+    }
+
+    return stack.length === 0;
 };
 
 console.log(isValid('()'));// true
