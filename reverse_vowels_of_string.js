d49e21aa874919c1379cc112ce7b594854d97e57
RaymondwChen <70980012+RaymondwChen2@users.noreply.github.com>
2021-11-16 17:51:01 -0800

    leetcode

--- reverse_vowels_of_string.js
+++ reverse_vowels_of_string.js
@@ -1,5 +1,23 @@
 var reverseVowels = function(s) {
     let vowels = 'aeiou';
+    let queue = [];
+    let newStr = "";
+
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+            queue.push(s[i]);
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+            newStr += queue.pop();
+        } else {
+            newStr += s[i];
+        }
+    }
+
+    return newStr;
 };
 
 console.log(reverseVowels("hello")); // 'holle'
