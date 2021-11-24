9f8b91b0a53a8cf360be98b8e040bc0c52075e1f
RaymondwChen <70980012+RaymondwChen2@users.noreply.github.com>
2021-11-23 19:50:19 -0800

    leetcode

--- single_number.js
+++ single_number.js
@@ -1,5 +1,19 @@
 var singleNumber = function(nums) {
+    let obj = {};
 
+    for (let i = 0; i < nums.length; i++) {
+        if (obj[nums[i]] === undefined) {
+            obj[nums[i]] = 1;
+        } else {
+            obj[nums[i]] += 1;
+        }
+    }
+
+    for (let key in obj) {
+        if (obj[key] === 1) {
+            return key;
+        }
+    }
 };
 
 console.log(singleNumber([2, 2, 1])); // 1
